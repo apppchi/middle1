a2ade8c62edba67992adfc87934ade49dde9fa23
apppchi <112154499+apppchi@users.noreply.github.com>
2022-12-16 15:25:47 +0300

    Update function_4.cpp

--- middle1/function_4.cpp
+++ middle1/function_4.cpp
@@ -2,6 +2,15 @@
 
 int itc_rev_oct_num(long long number) {
     long long a = itc_oct_num(number);
-    long long b = itc_rev_num(a);
+    long long b = itc_rev_ch(a);
     return b;
 }
+
+int itc_rev_ch(long long number) {
+    long long c = 0;
+    if (number < 0)
+        number = number * -1;
+    while (number > 0) {
+        c = c * 10 + number % 10;
+        number = number / 10;
+    }
