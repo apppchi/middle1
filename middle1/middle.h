5a31e57fc77a9a308be4e4649f266880ab6cfb40
apppchi <112154499+apppchi@users.noreply.github.com>
2022-12-16 15:27:56 +0300

    Update middle.h

--- middle1/middle.h
+++ middle1/middle.h
@@ -21,5 +21,6 @@ long long itc_bin_num(long long number);
 long long itc_oct_num(long long number);
 int itc_rev_bin_num(long long number);
 int itc_rev_oct_num(long long number);
+int itc_rev_ch(long long number);
 
 #endif // MIDDLE_H_INCLUDED
