bee1a2ed0be0098ff2cc5413034cc6c0e3658419
apppchi <112154499+apppchi@users.noreply.github.com>
2022-12-16 15:21:50 +0300

    Update function_2.cpp

--- middle1/function_2.cpp
+++ middle1/function_2.cpp
@@ -18,13 +18,13 @@ int itc_min_num(long long number){
 
 int itc_rev_num(long long number) {
     long long c = 0;
-	if (number < 0)
+    if (number < 0)
         number = number * -1;
     while (number > 0) {
         c = c * 10 + number % 10;
         number = number / 10;
     }
-    return c;
+    return itc_len_num(c);
 }
 
 int itc_null_count(long long number){
